ed092b74149a3a533b52a27732df780bac282fcb
Satya-098 <satyabeshra@gmail.com>
2025-08-29 09:47:22 +0530

    Add to commit

--- sample.java
+++ sample.java
@@ -2,5 +2,7 @@ public class sample {
     public static void main(String args[])
     {
         System.out.println("Git hub training");
+        System.out.println("This is from a new feature by:- SATYANARAYAN");
+        
     }
 }

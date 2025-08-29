c8dac777f42e40a443047676ec32109a28d6f6ba
Satya-098 <satyabeshra@gmail.com>
2025-08-29 09:48:46 +0530

    Made some changes in sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,5 @@
-public class sample {
-    public static void main(String args[])
-    {
-        System.out.println("Git hub training");
-        System.out.println("This is from a new feature by:- SATYANARAYAN");
-        
+public class Sample{
+    public static void main(String[] args) {
+        System.out.println("Hello");
     }
 }
